7a05fc3616daa4d032facaace84154cb5e9deb82
Ethan Wiens <ethanjwiens@gmail.com>
2024-05-30 21:58:24 -0500

    theme change

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -8,13 +8,10 @@ vim.opt.number = true
 vim.opt.numberwidth = 2
 vim.opt.ruler = false
 
+vim.cmd("set textwidth=75")
+vim.opt.wrap=true
+
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
-
-
---set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
---  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
---  \,sm:block-blinkwait175-blinkoff150-blinkon175
