9f68f3f282b5d5cd0f33660887a4e529188e2cb9
Ethan Wiens <ethanjwiens@gmail.com>
2024-06-13 17:37:37 -0500

    update

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -20,22 +20,25 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+      lspconfig.eslint.setup({
+        capabilities = capabilities
+      })
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.pyright.setup({
 				capabilities = capabilities
 			})
 			lspconfig.markdown_oxide.setup({
 				capabilities = capabilities
 			})
-      lspconfig.eslint.setup({
-        capabilities = capabilities
-      })
       lspconfig.omnisharp.setup({
         capabilities = capabilities,
         cmd = { "dotnet", "/Users/ethanwiens/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll"},
       })
+			lspconfig.pyright.setup({
+				capabilities = capabilities
+			})
+			lspconfig.rubocop.setup({
+				capabilities = capabilities
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
