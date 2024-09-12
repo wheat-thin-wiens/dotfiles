d9edf057e321483e49a6d14bb01d437795ca4eaa
Ethan Wiens <ethanwiens@airbud-2-pawppin-off.local>
2024-09-12 12:30:06 -0500

    fixed starship, updated directories in theme script

--- theme-picker.sh
+++ theme-picker.sh
@@ -1,11 +1,14 @@
 #!/bin/zsh
 
 function colorpick {
-  cd ~/.config
+  cd ~/dotfiles/starship/.config
   rm -f starship.toml
   cd -
-  cd ~/.config/starship_themes
+  cd ~/dotfiles/starship/.config/starship_themes
   cp starship_$1.toml ~/.config/starship.toml
+  cd -
+  cd ~/dotfiles
+  stow starship
   cd -
 
   nvim ~/.config/nvim/lua/theme-picker.lua
