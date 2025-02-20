34330c631a2d1eef49d544ad3323fa3758907508
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-02-19 20:49:52 -0600

    2-19 update

--- sddm/install.sh
+++ sddm/install.sh
@@ -3,7 +3,7 @@
 sudo rm -rf /etc/sddm.conf
 sudo ln -s ~/dotfiles/sddm/etc/sddm.conf /etc/sddm.conf
 
-sudo rm -rf /usr/share/sddm/themes
-sudo ln -s ~/dotfiles/sddm/usr/share/sddm/themes /usr/share/sddm/themes
+# sudo rm -rf /usr/share/sddm/themes
+# sudo ln -s ~/dotfiles/sddm/usr/share/sddm/themes /usr/share/sddm/themes
 
 sudo cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
