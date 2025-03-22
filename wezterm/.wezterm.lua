14b96122181c85db225ad80d7b232f3413755915
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-03-22 17:04:43 -0500

    3/22 update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,6 +21,7 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 30
 
+config.window_close_confirmation = "NeverPrompt"
 config.enable_wayland = false
 config.warn_about_missing_glyphs = false
 
